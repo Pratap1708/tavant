a21f38850795ac4c70d7757e7642f74ce0b5b99a
Saurabh Saurabh <ssaurabh2@sapient.com>
2017-04-22 19:27:41 +0530

    Leave Application Separate SCSS module added JS code description added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
+/*
+ * @project:    Leave Application
+ * @date:       2017-04-20
+ * @author:     Saurabh
+ * @namespaces: LA
+ */
+
+
 window.LA = window.LA || {};
 "use strict";
 LA.Controller = (function (context) {
+    
+	/**
+	Variables
+	*/
     var leavesData = [],
         daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
         $el = {};
 
-     var _initDOMCaching,
+	/**
+	 Methods
+	 */
+    var _initDOMCaching,
      	_initDOMEvents,
      	_submitLeaveForm,
      	_enableSubmit,
@@ -18,7 +33,11 @@ LA.Controller = (function (context) {
      	_checkHalfDay,
      	_changeLeaveType;
 
-
+    /**
+     * @method _initDOMCaching
+     * @description DOM Caching in Variables
+     * @memberof LA.Controller
+     */
     _initDOMCaching = function() {
         $el.leaveForm = $('.leave-form');
         $el.leaveBucketForm = $('.leave-bucket-form');
@@ -27,6 +46,11 @@ LA.Controller = (function (context) {
         $el.searchEntries = $('#search');
     }
 
+    /**
+     * @method _initDOMEvents
+     * @description Event Binding
+     * @memberof LA.Controller
+     */
     _initDOMEvents = function() {
         $el.leaveForm.off('submit.leaveForm').on('submit.leaveForm', _submitLeaveForm);
         $el.leaveBucketForm.off('submit.leaveBucketForm').on('submit.leaveBucketForm', _submitleaveBucketForm);
@@ -37,6 +61,11 @@ LA.Controller = (function (context) {
         $el.searchEntries.off('keyup').on('keyup', _searchEntries);
     }
 
+    /**
+     * @method _submitLeaveForm
+     * @description Submit Leave form 
+     * @memberof LA.Controller
+     */
     _submitLeaveForm = function(event) {
         event.preventDefault();
         var startDate = $el.leaveForm.find('#startDate').val(),
@@ -67,6 +96,11 @@ LA.Controller = (function (context) {
         _renderLeaveBucket(leavesData);
     }
 
+    /**
+     * @method _enableSubmit
+     * @description Check when Submit Button need to be enabled
+     * @memberof LA.Controller
+     */
     _enableSubmit = function(event) {
     	if ($('.leave-table tbody tr').length > 0) {
     		$el.leaveBucketForm.find('.btn-primary').removeClass('disabled');
@@ -75,6 +109,11 @@ LA.Controller = (function (context) {
     	}
     }
 
+    /**
+     * @method _submitleaveBucketForm
+     * @description Final Ajax Call to submit Leave Bucket Form
+     * @memberof LA.Controller
+     */
     _submitleaveBucketForm = function(event) {
     	if ($(this).find('.btn-primary').hasClass('disabled')) {
     		return;
@@ -93,10 +132,20 @@ LA.Controller = (function (context) {
         })
     }
 
+    /**
+     * @method _getNoOfDays
+     * @description Calculation of days based on the Start and End Date of Leave
+     * @memberof LA.Controller
+     */
     _getNoOfDays = function(startDate, endDate) {
         return ((endDate - startDate) / (1000 * 60 * 60 * 24));
     }
 
+    /**
+     * @method _renderLeaveBucket
+     * @description Leave Bucket Rendering on submission of apply leave form
+     * @memberof LA.Controller
+     */
     _renderLeaveBucket = function(data, howMany) {
         var tableString = '<tr><td>{date}</td><td>{day}</td><td class="text-center"><input type="checkbox" class="halfday" name="halfday"></td><td><select class="leave-type" id="leaveType" name="leaveType"><option selected="selected">PL</option><option>CL</option><option>SL</option></select></td><td class="text-center"><a href="#" class="tavant-icon tavant-delete"></a></td></tr>',
             temp;
@@ -111,6 +160,11 @@ LA.Controller = (function (context) {
         _enableSubmit();
     }
 
+    /**
+     * @method _showEntries
+     * @description Shows the number of entries to be displayed on page
+     * @memberof LA.Controller
+     */
     _showEntries = function (event) {
         var value = parseInt($(this).val());
         if (isNaN(value)) {
@@ -120,6 +174,11 @@ LA.Controller = (function (context) {
         }
     }
 
+    /**
+     * @method _cancelLeave
+     * @description Delete a specific day's leave
+     * @memberof LA.Controller
+     */
     _cancelLeave = function(event) {
         var $this = $(this),
             index;
@@ -129,17 +188,27 @@ LA.Controller = (function (context) {
         $el.showEntries.trigger('change');
     }
 
+    /**
+     * @method _searchEntries
+     * @description Seach on the basis of Day's Name
+     * @memberof LA.Controller
+     */
     _searchEntries = function() {
         var value = $(this).val().toLowerCase(),
             data,
             regexp = new RegExp(value, 'g');
-        data = leavesData.filter(function(obj) {
-            return regexp.test(obj.day.toLowerCase())
-        });
+        	data = leavesData.filter(function(obj) {
+            	return regexp.test(obj.day.toLowerCase());
+        	});
         $el.showEntries.val('');
         _renderLeaveBucket(data);
     }
 
+    /**
+     * @method _checkHalfDay
+     * @description Check any given day leave is half day or full
+     * @memberof LA.Controller
+     */
     _checkHalfDay = function() {
         var $this = $(this),
             index;
@@ -147,6 +216,11 @@ LA.Controller = (function (context) {
         leavesData[index].halfDay = $this.prop('checked');
     }
 
+    /**
+     * @method _changeLeaveType
+     * @description Leave Type change capturing in Leave Bucket
+     * @memberof LA.Controller
+     */
     _changeLeaveType = function() {
         var $this = $(this),
             index;
@@ -154,12 +228,18 @@ LA.Controller = (function (context) {
         leavesData[index].leaveType = $this.val();
     }
 
+    /**
+     * @method context.init
+     * @description Initializes on Page Load and binds the necessary methods
+     * @memberof LA.Controller
+     */
     context.init = function() {
         _initDOMCaching();
         _initDOMEvents();
         _enableSubmit();
     }
 
+    // Public API
     return context;
 })({});
 
